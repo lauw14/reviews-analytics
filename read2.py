1a60e23d63974c613815df1f6f962d55d90a7c14
lauw14 <lauw14@hotmail.com>
2022-12-26 19:33:34 +0800

    filter

--- read2.py
+++ read2.py
@@ -8,15 +8,19 @@ with open('reviews.txt', 'r') as f:#with會執行open，並且自動close
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
-		#strip只能針對字串，append只能針對清單
-print(data[0])
-print('--------------------------')
-print(data[1])
-print('檔案讀取完了，總共有', len(data),'筆資料')
-
 sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)  #用for迴圈總合留言長度
 print('全部留言的總長度為', sum_len)
 avg_len = sum_len / len(data)
 print('每筆留言的平均長度為', avg_len)
+
+new = [] #新的清單陣列
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('小於100字的，有 ', len(new), '筆')
+print(new[0])
+print(new[1])
+
+	
